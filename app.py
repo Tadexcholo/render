65e0ee6be2f05841bdfd2debd53e8db2ad7d74cb
Tadexcholo <166573864+Tadexcholo@users.noreply.github.com>
2024-09-12 19:41:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,24 +5,41 @@ from flask import request
 
 import pusher
 
+import mysql.connector
+import datetime
+import pytz
+
+con = mysql.connector.connect(
+  host="185.232.14.52",
+  database="u760464709_tst_sep",
+  user="u760464709_tst_sep_usr",
+  password="dJ0CIAFF="
+)
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    con.close()
     return render_template("app.html")
 
 @app.route("/alumnos")
 def alumnos():
+    con.close()
     return render_template("alumnos.html")
 
 @app.route("/alumnos/guardar", methods=["POST"])
 def alumnosGuardar():
+    con.close()
     matricula      = request.form["txtMatriculaFA"]
     nombreapellido = request.form["txtNombreApellidoFA"]
     return f"Matrícula: {matricula} Nombre y Apellido: {nombreapellido}"
 
 @app.route("/evento", methods=["GET"])
 def evento():
+    if not con.is_connected():
+        con.reconnect()
+
     pusher_client = pusher.Pusher(
         app_id="1714541",
         key="cda1cc599395d699a2af",
