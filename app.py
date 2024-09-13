62052917b1bf14611dcbdc7b2f801d44ee9df8a8
Tadexcholo <166573864+Tadexcholo@users.noreply.github.com>
2024-09-12 18:13:46 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 from flask import render_template
 from flask import request
-
+import pusher
 app = Flask(__name__)
 
 @app.route("/")
@@ -18,3 +18,16 @@ def alumnosGuardar():
     matricula = request.form["txtMatriculaFA"];
     nombreapellido = request.form["txtNombreApellidoFA"];
     return f"Matrícula: {matricula} Nombre y Apellido: {nombreapellido}"
+@app.route("/evento")
+def evento()
+
+
+pusher_client = pusher.Pusher(
+  app_id='1767944',
+  key='93408f3e56875b1bb947',
+  secret='fd213855fc0b2521c437',
+  cluster='us2',
+  ssl=True
+)
+
+pusher_client.trigger('conexion', 'evento', {'message': 'hello world'})
