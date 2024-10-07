5fa71dbf267cdf0ca46e96070d9596cef0665b0d
Tadexcholo <166573864+Tadexcholo@users.noreply.github.com>
2024-10-07 17:29:21 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
+# python.exe -m venv .venv
+# cd .venv/Scripts
+# activate.bat
+# py -m ensurepip --upgrade
+
 from flask import Flask
-from markupsafe import escape
+
 from flask import render_template
 from flask import request
+from flask import jsonify, make_response
+
 import pusher
+
 import mysql.connector
 import datetime
 import pytz
 
 con = mysql.connector.connect(
-  host="185.232.14.52",
-  database="u760464709_tst_sep",
-  user="u760464709_tst_sep_usr",
-  password="dJ0CIAFF="
+    host="185.232.14.52",
+    database="u760464709_tst_sep",
+    user="u760464709_tst_sep_usr",
+    password="dJ0CIAFF="
 )
 
 app = Flask(__name__)
@@ -19,58 +27,124 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     con.close()
-  
+
     return render_template("app.html")
 
 @app.route("/alumnos")
 def alumnos():
     con.close()
-  
+
     return render_template("alumnos.html")
 
 @app.route("/alumnos/guardar", methods=["POST"])
 def alumnosGuardar():
     con.close()
-  
     matricula      = request.form["txtMatriculaFA"]
     nombreapellido = request.form["txtNombreApellidoFA"]
 
-    return f"Matrícula: {matricula} Nombre y Apellido: {nombreapellido}"
+    return f"Matrícula {matricula} Nombre y Apellido {nombreapellido}"
 
-@app.route("/registrar", methods=["GET"])
-def registrar():
-    args = request.args
+# Código usado en las prácticas
+def notificarActualizacionTemperaturaHumedad():
     pusher_client = pusher.Pusher(
-      app_id='1869080',
-      key='cdcefdd16aec9a6423e7',
-      secret='5d8e0749eed8b7b71f31',
-      cluster='us2',
-      ssl=True
+        app_id='1869080',
+        key='cdcefdd16aec9a6423e7',
+        secret='5d8e0749eed8b7b71f31',
+        cluster='us2',
+        ssl=True
     )
 
-    if not con.is_connected():
-        con.reconnect()
-    cursor = con.cursor()
-    
-    sql = "INSERT INTO tst0_cursos_pagos (Telefono, Archivo) VALUES (%s, %s)"
-    val = (args["Telefono"], args["Archivo"])
-    cursor.execute(sql, val)
-
-    con.commit()
-    con.close()
- 
-    pusher_client.trigger("registrosTiempoReal", "registroTiempoReal", args)
-    return args
+    pusher_client.trigger("canalRegistrosTemperaturaHumedad", "registroTemperaturaHumedad", args)
 
 @app.route("/buscar")
 def buscar():
     if not con.is_connected():
         con.reconnect()
-    cursor = con.cursor()
-    cursor.execute("SELECT * FROM tst0_cursos_pagos ORDER BY Id_Curso_Pago DESC")
+
+    cursor = con.cursor(dictionary=True)
+    cursor.execute("""
+    SELECT Id_Curso_Pago, Telefono, Archivo FROM tst0_cursos_pagos
+    ORDER BY Id_Curso_Pago DESC
+    LIMIT 10 OFFSET 0
+    """)
     registros = cursor.fetchall()
 
     con.close()
 
-    return registros
-  
+    return make_response(jsonify(registros))
+
+@app.route("/guardar", methods=["POST"])
+def guardar():
+    if not con.is_connected():
+        con.reconnect()
+
+    id          = request.form["id"]
+    telefono = request.form["telefono"]
+    archivo     = request.form["archivo"]
+    
+    cursor = con.cursor()
+
+    if id:
+        sql = """
+        UPDATE tst0_cursos_pagos SET
+        Telefono = %s,
+        Archivo     = %s
+        WHERE Id_Curso_Pago = %s
+        """
+        val = (temperatura, humedad, id)
+    else:
+        sql = """
+        INSERT INTO tst0_cursos_pagos (Telefono, Archivo)
+                        VALUES (%s,          %s,      %s)
+        """
+        val =                  (Telefono, Archivo)
+    
+    cursor.execute(sql, val)
+    con.commit()
+    con.close()
+
+    notificarActualizacionTemperaturaHumedad()
+
+    return make_response(jsonify({}))
+
+@app.route("/editar", methods=["GET"])
+def editar():
+    if not con.is_connected():
+        con.reconnect()
+
+    id = request.args["id"]
+
+    cursor = con.cursor(dictionary=True)
+    sql    = """
+    SELECT Id_Curso_Pago, Telefono, Archivo FROM tst0_cursos_pagos
+    WHERE Id_Curso_Pago = %s
+    """
+    val    = (id,)
+
+    cursor.execute(sql, val)
+    registros = cursor.fetchall()
+    con.close()
+
+    return make_response(jsonify(registros))
+
+@app.route("/eliminar", methods=["POST"])
+def eliminar():
+    if not con.is_connected():
+        con.reconnect()
+
+    id = request.form["id"]
+
+    cursor = con.cursor(dictionary=True)
+    sql    = """
+    DELETE FROM tst0_cursos_pagos
+    WHERE Id_Curso_Pago = %s
+    """
+    val    = (id,)
+
+    cursor.execute(sql, val)
+    con.commit()
+    con.close()
+
+    notificarActualizacionTemperaturaHumedad()
+
+    return make_response(jsonify({})
